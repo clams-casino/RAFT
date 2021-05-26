ed1d86f986e86d06404d3d5fb772cc1ebbf881c5
mike <mike.zhang.619@gmail.com>
2021-05-26 17:24:41 +0200

    remove visualization

--- evaluate.py
+++ evaluate.py
@@ -96,34 +96,10 @@ def create_mhof_submission(model, iters=24, output_path='mhof_submission'):
         
         flow_sub = flow_sub.squeeze(0).permute(1, 2, 0)
         flow_sub = flow_sub.cpu().numpy()
-        print(flow_sub.shape)
-        #TODO visualize this
-
-        vis = flow2rgb(flow_sub)
-        plt.imshow(vis)
-        plt.show()
 
         output_filename = os.path.join(output_path, test_out)
-        print(output_filename)
-
+        
         # frame_utils.writeFlow(output_filename, flow, encoding=np.float16) 
-
-
-def flow2rgb(flow_map, max_value=None): #NOTE this one takes in a numpy array
-    flow_map_np = flow_map.transpose(2,0,1)
-    _, h, w = flow_map_np.shape
-    flow_map_np[:,(flow_map_np[0] == 0) & (flow_map_np[1] == 0)] = float('nan')
-    rgb_map = np.ones((3,h,w)).astype(np.float32)
-    if max_value is not None:
-        normalized_flow_map = flow_map_np / max_value
-    else:
-        normalized_flow_map = flow_map_np / (np.abs(flow_map_np).max())
-    rgb_map[0] += normalized_flow_map[0]
-    rgb_map[1] -= 0.5*(normalized_flow_map[0] + normalized_flow_map[1])
-    rgb_map[2] += normalized_flow_map[1]
-    rgb_flow = rgb_map.clip(0,1)
-    rgb_flow = (rgb_flow * 255).astype(np.uint8).transpose(1,2,0)
-    return rgb_flow
 
 
 @torch.no_grad()
