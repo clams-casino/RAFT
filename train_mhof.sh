278079cf66f8046fc7ded8e893f5d1544f37ec07
mike <mike.zhang.619@gmail.com>
2021-05-26 19:18:23 +0200

    shell script for running the training on mhof

---
+++ train_mhof.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+mkdir -p checkpoints
+python3 -u train.py --name raft-mhof --stage mhof --validation mhof --restore_ckpt checkpoints/raft-things.pth --gpus 0 --num_steps 100000 --batch_size 6 --lr 0.000125 --image_size 448 320 --wdecay 0.00001 --gamma=0.85
