ae83bce6da7297a7b3ad505e131bbb1b6a74771f
mike <mike.zhang.619@gmail.com>
2021-06-17 21:55:49 +0200

    increase batch size to 12

--- train_mhof.sh
+++ train_mhof.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 mkdir -p checkpoints
-python3 -u train.py --name raft-mhof --stage mhof --validation mhof --restore_ckpt models/raft-things.pth --gpus 0 --num_steps 200000 --batch_size 6 --lr 0.000125 --image_size 448 320 --wdecay 0.00001 --gamma=0.85 --mixed_precision
+python3 -u train.py --name raft-mhof --stage mhof --validation mhof --restore_ckpt models/raft-things.pth --gpus 0 --num_steps 200000 --batch_size 12 --lr 0.000125 --image_size 448 320 --wdecay 0.00001 --gamma=0.85 --mixed_precision
